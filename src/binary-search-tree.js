779e7eb1c13610c0691a057234ac48159b5e39c3
Anton Ustyuzhanin <au@hellofresh.com>
2021-10-15 20:14:59 +0200

    feat: Binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,124 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+function bstAdd(root, data) {
+  if (root === null) {
+    return new Node(data);
+  }
+  if (root.data > data) {
+    root.left = bstAdd(root.left, data)
+  } else if (root.data < data) {
+    root.right = bstAdd(root.right, data)
+  }
+  return root;
+}
+
+function bstHas(root, data) {
+  if (root === null) {
+    return false;
+  }
+  if (root.data === data) {
+    return true;
+  }
+  if (root.data > data) {
+    return bstHas(root.left, data)
+  }
+  return bstHas(root.right, data)
+}
+
+function bstFind(root, data) {
+  if (root === null || root.data === data) {
+    return root;
+  }
+  if (root.data > data) {
+    return bstFind(root.left, data)
+  }
+  return bstFind(root.right, data)
+}
+
+function bstMin(root) {
+  if (root.left !== null) {
+    return bstMin(root.left);
+  }
+  return root.data;
+}
+
+function bstRemove(root, data) {
+  if (root === null)
+    return root;
+
+  if (root.data > data) {
+    root.left = bstRemove(root.left, data);
+  } else if (root.data < data) {
+    root.right = bstRemove(root.right, data);
+  } else {
+
+    if (root.left == null) { return root.right; }
+    else if (root.right == null) { return root.left; }
+
+    root.data = bstMin(root.right);
+    root.right = bstRemove(root.right, root.data);
+  }
+
+  return root;
+}
+
+function bstMax(root) {
+  if (root.right !== null) {
+    return bstMax(root.right);
+  }
+  return root.data;
+}
+
+function bstInorder(root, res = []) {
+  if (root !== null) {
+    bstInorder(root.left, res);
+    res.push(root.data);
+    bstInorder(root.right, res);
+  }
+  return res;
+}
+
 module.exports = class BinarySearchTree {
 
+  _root = null;
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this._root = bstAdd(this._root, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return bstHas(this._root, data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return bstFind(this._root, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this._root = bstRemove(this._root, data);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return bstMin(this._root);
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return bstMax(this._root);
+  }
+
+  inorder() {
+    return bstInorder(this._root);
   }
 
 }
