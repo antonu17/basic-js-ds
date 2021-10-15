483096a8c444877d9c0c9fea88955ed2e12c8d76
Anton Ustyuzhanin <au@hellofresh.com>
2021-10-15 20:15:59 +0200

    feat: (ST) Remove from list

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+  while (l.value === k) {
+    l = l.next;
+  }
+
+  let head = l;
+
+  while (head.next !== null) {
+    if (head.next.value === k) {
+      head.next = head.next.next;
+    }
+    head = head.next;
+  }
+
+  return l;
 }
