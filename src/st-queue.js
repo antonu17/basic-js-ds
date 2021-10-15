03e01ab02768d990c15ed4655a6fb50ac44b99b1
Anton Ustyuzhanin <au@hellofresh.com>
2021-10-15 20:18:06 +0200

    feat: (ST) Queue

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+ function insert(list, data) {
+  if (list === null) {
+    return new ListNode(data);
+  } else {
+    list.next = insert(list.next, data);
+  }
+  return list;
+}
+
 module.exports = class Queue {
 
+  queue = null;
+
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    this.queue = insert(this.queue, value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.queue == null) {
+      return null;
+    }
+    let value = this.queue.value;
+    this.queue = this.queue.next;
+    return value;
   }
-
 }
